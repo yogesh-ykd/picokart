b2cadc1b217f23bbb0b984f8c58fac40bfae9121
adikalra <54812001+AdiKalra@users.noreply.github.com>
2024-10-12 17:48:13 +0530

    Added Static Files configuration

--- picokart/picokart/settings.py
+++ picokart/picokart/settings.py
@@ -130,6 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')  # Location to save the static files
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'AffiliateMarketing/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
